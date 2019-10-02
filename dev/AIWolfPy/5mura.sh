b1b077ee82beb1edea638f075d786c378c16f0fb
ebifly <plmwer789@gmail.com>
2019-10-03 01:11:39 +0900

    news

---
+++ dev/AIWolfPy/5mura.sh
@@ -0,0 +1,5 @@
+python3 agent.py -h localhost -p 10000 &
+python3 agent.py -h localhost -p 10000 &
+python3 agent.py -h localhost -p 10000 &
+python3 agent.py -h localhost -p 10000 &
+#python3 agent.py -h localhost -p 10000 &
