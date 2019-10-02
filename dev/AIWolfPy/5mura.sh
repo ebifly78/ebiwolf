d37a58045bc2d2fa5c2bff05b6478df0705df4d7
ebifly <plmwer789@gmail.com>
2019-10-03 01:08:26 +0900

    cached

--- dev/AIWolfPy/5mura.sh
+++
@@ -1,5 +0,0 @@
-python3 agent.py -h localhost -p 10000 &
-python3 agent.py -h localhost -p 10000 &
-python3 agent.py -h localhost -p 10000 &
-python3 agent.py -h localhost -p 10000 &
-#python3 agent.py -h localhost -p 10000 &
